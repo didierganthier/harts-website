544269735fa1b924c263f1bc123bbdd8f2fdf0bd
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-12-07 21:28:19 -0500

    Fix linters errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/forbid-prop-types */
+import '../styles/globals.css';
+import PropTypes from 'prop-types';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
+
+// props validation
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
