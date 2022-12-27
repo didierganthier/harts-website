d764f66ef42128015f83470b6a7022a99ca54a8c
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-12-27 18:59:36 -0500

    Add image to Discover Component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+import Head from 'next/head';
 import Image from 'next/image';
+import discoverImg from '../assets/discover-img.png';
 import { useState } from 'react';
 
 export default function Home() {
@@ -7,7 +9,7 @@ export default function Home() {
 
   const [current, setCurrent] = useState(1);
 
-  const [navLinks, setNavLinks] = useState([
+  const navLinks = [
     {
       id: 1,
       name: 'Home',
@@ -33,11 +35,14 @@ export default function Home() {
       name: 'Contact',
       url: '#contact',
     },
-  ]);
+  ];
 
   return (
-    <div>
-      <nav className="bg-[#222222] border-gray-200 px-2 sm:px-4 py-2.5">
+    <div className='bg-[#222222]'>
+      <Head>
+        <title>Hart&apos;s</title>
+      </Head>
+      <nav className="border-gray-200 px-2 sm:px-4 py-2.5">
         <div className="container flex flex-wrap items-center justify-between mx-auto">
           <a href="#" className="flex items-center">
             <Image src="/./hartslogo.png" className="h-6 mr-3 sm:h-9" alt="Hart's Logo" width="100" height="100" />
@@ -50,18 +55,26 @@ export default function Home() {
             <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-[#222222] md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
               {navLinks.map((link) => (
                 <li key={link.id} onClick={() => setCurrent(link.id)}>
-                  <a href={link.url} className={`block py-2 pl-3 pr-4 text-white ${current === link.id ? "bg-[#B88222]": ""} rounded md:bg-transparent`}>{link.name}</a>
+                  <a href={link.url} className={`block py-2 pl-3 pr-4 text-white ${current === link.id ? "bg-[#B88222]" : ""} rounded md:bg-transparent`}>{link.name}</a>
                 </li>
               ))}
             </ul>
           </div>
         </div>
       </nav>
-      <p className=''>Cultural career and community management</p>
-      <p>POUR UNE MEILLEURE VALORISATION DE NOS JEUNES TALENTS.</p>
-      <button>
-      DISCOVER
-      </button>
+      <div className='mx-4 my-8'>
+        <p className='uppercase text-2xl pb-2 font-extrabold text-[#B88222]'>Cultural career and community management</p>
+        <p className=' text-white text-[19px]'>POUR UNE MEILLEURE VALORISATION DE NOS JEUNES TALENTS.</p>
+        <p className='text-[4px] text-white py-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum feugiat consequat. Suspendisse est arcu, iaculis nec imperdiet ac, mollis varius augue. Aliquam in consequat arcu. Donec luctus tempor dui, sed rutrum turpis dignissim ut. Sed placerat dui dolor, nec vulputate urna rhoncus vel. Aenean ut est aliquam enim vestibulum dapibus. Fusce at condimentum tellus, vitae porttitor nibh. Aliquam faucibus auctor metus sed ullamcorper. Ut blandit</p>
+        <button className='text-white bg-[#B88222] text-xs flex items-center rounded-full px-2 py-1'>
+          DISCOVER
+          <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
+        </button>
+        {/* Discover Image */}
+      </div>
+      <div className='flex'>
+        <Image src={discoverImg} className="w-full" alt="Discover Image" />
+      </div>
     </div>
   )
 }
