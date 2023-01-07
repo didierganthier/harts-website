d1d276a1896643ddebded6d878cade73bc2a1d99
Desor Guishny <desorguishny@gmail.com>
2023-01-07 14:57:50 -0500

    Update community component for desktop version

--- components/CommunityComponent.js
+++ components/CommunityComponent.js
@@ -5,15 +5,15 @@ import ButtonForward from './ButtonForward';
 
 const CommunityComponent = () => {
     return (
-        <div className='bg-white md:flex md:flex-row-reverse items-center'>
-            <div className='mx-4 py-8'>
+        <div className='bg-white md:flex md:flex-row-reverse items-center w-full'>
+            <div className='mx-4 md:px-12 py-8'>
                 <p className='pb-1 font-bold uppercase text-[12px] text-[#B88222]'>Our community</p>
                 <p className='uppercase text-2xl pb-2 font-extrabold'>Having a network of creative</p>
                 <p className='text-[8px] py-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum feugiat consequat. Suspendisse est arcu, iaculis nec imperdiet ac, mollis varius augue. Aliquam in consequat arcu. Donec luctus tempor dui, sed rutrum turpis dignissim ut. Sed placerat dui dolor, nec vulputate urna rhoncus vel. Aenean ut est aliquam enim vestibulum dapibus. Fusce at condimentum tellus, vitae porttitor nibh. Aliquam faucibus auctor metus sed ullamcorper. Ut blandit</p>
                 <ButtonForward textChildren="Discover more" />
             </div>
-            <div className='flex'>
-                <Image src={discoverImg} className="w-[80%] rounded-r-2xl" alt="Discover Image" />
+            <div className='flex md:block w-2/3 rounded-r-2xl md:rounded-r-3xl overflow-hidden'>
+                <Image src={discoverImg} className="w-full min-h-screen object-cover" alt="Discover Image" />
             </div>
         </div>
     )
